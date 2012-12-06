1386ac532e498492957cdfdf1e2fd5eb7df9f747
Iskander Haziev <gvalmon@gmail.com>
2012-12-06 15:22:14 +0400

    update attribute restrictions

--- lib/encoding_dot_com/attribute_restrictions.rb
+++ lib/encoding_dot_com/attribute_restrictions.rb
@@ -20,11 +20,11 @@ module EncodingDotCom
   module AttributeRestrictionsFlv #:nodoc:
 #    validate_inclusion_of :audio_bitrate, %w{32k 40k 48k 56k 64k 80k 96k 112k 128k 144k 160k 192k 224k 256k 320k}
 #    validate_inclusion_of :video_codec, %w{flv libx264 vp6}
-    
+
     def validate_audio_bitrate
       allowed_bitrates = %w{32k 40k 48k 56k 64k 80k 96k 112k 128k 144k 160k 192k 224k 256k 320k}
     end
-    
+
     def validate_video_codec
       allowed_codecs = %w{flv libx264 vp6}
       unless video_codec.nil? || allowed_codecs.include?(video_codec)
@@ -50,34 +50,37 @@ module EncodingDotCom
 
   module AttributeRestrictions3gp #:nodoc:
   end
-  
+
   module AttributeRestrictionsM4v #:nodoc:
   end
 
   module AttributeRestrictionsIphone #:nodoc:
   end
-  
+
   module AttributeRestrictionsIphone_stream #:nodoc:
   end
-  
+
   module AttributeRestrictionsIpad #:nodoc:
   end
-  
+
   module AttributeRestrictionsIpad_stream #:nodoc:
   end
-  
+
   module AttributeRestrictionsAppletv #:nodoc:
   end
 
   module AttributeRestrictionsPsp #:nodoc:
   end
-  
+
   module AttributeRestrictionsMp3 #:nodoc:
   end
-  
+
+  module AttributeRestrictionsWebm #:nodoc:
+  end
+
   module AttributeRestrictionsWma #:nodoc:
   end
-  
+
   module AttributeRestrictionsThumbnail #:nodoc:
   end
 end
